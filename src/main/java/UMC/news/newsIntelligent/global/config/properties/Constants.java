1d9126206ac44a5c3dccae81c167e7263ef053de
caminobelllo <alstpgml0812@gmail.com>
2025-07-11 18:10:40 +0900

    Feat: jwt 헤더 상수 처리

---
+++ src/main/java/UMC/news/newsIntelligent/global/config/properties/Constants.java
@@ -0,0 +1,7 @@
+package UMC.news.newsIntelligent.global.config.properties;
+
+public class Constants {
+
+    public static final String AUTH_HEADER = "Authorization";
+    public static final String TOKEN_PREFIX = "Bearer ";
+}
