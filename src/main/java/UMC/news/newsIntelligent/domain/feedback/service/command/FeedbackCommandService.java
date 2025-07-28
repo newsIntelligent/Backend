17d0628270a7e8021d72a25a0b8979d4fc588019
donghyukseo <ehdgurdusdn@naver.com>
2025-07-29 00:09:06 +0900

    feat : FeedbackService 기능 구현

--- src/main/java/UMC/news/newsIntelligent/domain/feedback/service/command/FeedbackCommandService.java
+++ src/main/java/UMC/news/newsIntelligent/domain/feedback/service/command/FeedbackCommandService.java
@@ -1,4 +1,7 @@
 package UMC.news.newsIntelligent.domain.feedback.service.command;
 
+import UMC.news.newsIntelligent.domain.feedback.dto.FeedbackRequestDTO;
+
 public interface FeedbackCommandService {
+    void submitFeedback(FeedbackRequestDTO.FeedbackRequest request);
 }
