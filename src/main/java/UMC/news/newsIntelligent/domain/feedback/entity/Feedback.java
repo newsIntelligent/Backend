0687f05e5f5893d6852dd3cdf6bdaabff56d4e03
donghyukseo <ehdgurdusdn@naver.com>
2025-07-29 00:11:16 +0900

    refactor : Feedback entity 코드 refactoring

--- src/main/java/UMC/news/newsIntelligent/domain/feedback/entity/Feedback.java
+++ src/main/java/UMC/news/newsIntelligent/domain/feedback/entity/Feedback.java
@@ -1,6 +1,7 @@
-package UMC.news.newsIntelligent.domain.feedback;
+package UMC.news.newsIntelligent.domain.feedback.entity;
 
 import UMC.news.newsIntelligent.domain.member.Member;
+import UMC.news.newsIntelligent.global.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,10 +11,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "feedback")
-public class Feedback {
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Feedback extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
