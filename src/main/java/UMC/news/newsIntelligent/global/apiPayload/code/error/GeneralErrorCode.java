8a453eb9da73665a6addc5e47899e484aaf9e73c
caminobelllo <alstpgml0812@gmail.com>
2025-07-31 23:47:48 +0900

    Feat: 오류 응답 코드 추가

--- src/main/java/UMC/news/newsIntelligent/global/apiPayload/code/error/GeneralErrorCode.java
+++ src/main/java/UMC/news/newsIntelligent/global/apiPayload/code/error/GeneralErrorCode.java
@@ -26,7 +26,8 @@ public enum GeneralErrorCode implements BaseErrorCode{
     /* --- 회원/인증 관련 에러 ---*/
     OTP_WRONG      ( HttpStatus.BAD_REQUEST, "AUTH401", "인증번호가 일치하지 않습니다."),
     OTP_EXPIRED    ( HttpStatus.BAD_REQUEST, "AUTH402",  "인증번호가 만료되었습니다."),
-    ALREADY_DEACTIVATED ( HttpStatus.BAD_REQUEST, "MEMBER403", "이미 탈퇴한 계정입니다.");;
+    ALREADY_DEACTIVATED ( HttpStatus.BAD_REQUEST, "MEMBER403", "이미 탈퇴한 계정입니다."),
+    MEMBER_NOT_FOUND (HttpStatus.BAD_REQUEST, "MEMBER404", "존재하지 않는 회원입니다.");
 
     // 필요한 필드값 선언
     private final HttpStatus status;
