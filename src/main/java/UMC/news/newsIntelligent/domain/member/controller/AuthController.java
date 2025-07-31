86002792d37e533bbb1d011faffc81c3a4b85bcd
caminobelllo <alstpgml0812@gmail.com>
2025-07-31 22:11:46 +0900

    Refactor: 응답 통일

--- src/main/java/UMC/news/newsIntelligent/domain/member/controller/AuthController.java
+++ src/main/java/UMC/news/newsIntelligent/domain/member/controller/AuthController.java
@@ -6,6 +6,8 @@ import UMC.news.newsIntelligent.domain.mail.entity.OtpCode;
 import UMC.news.newsIntelligent.domain.member.dto.MemberResponseDto;
 import UMC.news.newsIntelligent.domain.member.dto.TokenResponseDto;
 import UMC.news.newsIntelligent.domain.member.service.AuthService;
+import UMC.news.newsIntelligent.global.apiPayload.CustomResponse;
+import UMC.news.newsIntelligent.global.apiPayload.code.success.GeneralSuccessCode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -20,19 +22,27 @@ public class AuthController {
 
     /* --- 메일 발송 --- */
     @PostMapping("/signup/email")
-    public void signupEmail(@RequestBody EmailRequestDto request) { auth.sendCode(request.email(), OtpCode.Type.SIGNUP);}
+    public CustomResponse<?> signupEmail(@RequestBody EmailRequestDto request) {
+        auth.sendCode(request.email(), OtpCode.Type.SIGNUP);
+        return CustomResponse.onSuccess(GeneralSuccessCode.EMAIL_SENT);
+    }
 
     @PostMapping("/login/email")
-    public void loginEmail(@RequestBody EmailRequestDto request) { auth.sendCode(request.email(), OtpCode.Type.LOGIN); }
+    public CustomResponse<?> loginEmail(@RequestBody EmailRequestDto request) {
+        auth.sendCode(request.email(), OtpCode.Type.LOGIN);
+        return CustomResponse.onSuccess(GeneralSuccessCode.EMAIL_SENT);
+    }
 
     /* --- 인증 코드 검증 --- */
     @PostMapping("/signup/verify")
-    public MemberResponseDto signupVerify(@RequestBody VerifyRequestDto request) {
-        return MemberResponseDto.from(auth.signupByCode(request.email(), request.code()));
+    public CustomResponse<MemberResponseDto>  signupVerify(@RequestBody VerifyRequestDto request) {
+        MemberResponseDto responseDto = MemberResponseDto.from(auth.signupByCode(request.email(), request.code()));
+        return CustomResponse.onSuccess(GeneralSuccessCode.SIGNUP_SUCCESS, responseDto);
     }
     @PostMapping("/login/verify")
-    public TokenResponseDto loginVerify(@RequestBody VerifyRequestDto request) {
-        return auth.loginByCode(request.email(), request.code());
+    public CustomResponse<TokenResponseDto> loginVerify(@RequestBody VerifyRequestDto request) {
+        TokenResponseDto responseDto =  auth.loginByCode(request.email(), request.code());
+        return CustomResponse.onSuccess(GeneralSuccessCode.LOGIN_SUCCESS, responseDto);
     }
 
     /* --- 매직 링크 --- */
