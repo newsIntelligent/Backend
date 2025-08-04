010566598945db37462df286ae442895c6fc9e1d
yoonseo8167 <yoonseo8167@sookmyung.ac.kr>
2025-08-04 14:48:47 +0900

    refactor: #45 필드 초기값 무시 경고 해결

--- src/main/java/UMC/news/newsIntelligent/domain/member/entity/Member.java
+++ src/main/java/UMC/news/newsIntelligent/domain/member/entity/Member.java
@@ -55,12 +55,14 @@ public class Member extends BaseEntity {
 		cascade = CascadeType.ALL,
 		orphanRemoval = true,
 		fetch = FetchType.LAZY)
+	@Builder.Default
 	private List<DailyReport> dailyReports = new ArrayList<>();
 
 	@OneToMany(
 		mappedBy = "member",
 		cascade = CascadeType.ALL,
 		orphanRemoval = true)
+	@Builder.Default
 	private List<MemberTopic> memberTopics = new ArrayList<>();
 
 	public static Member newMember(String email) {
