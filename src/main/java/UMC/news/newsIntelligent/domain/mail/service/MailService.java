ff80f844afb52d0713b589142dfd798e7dcd655c
caminobelllo <alstpgml0812@gmail.com>
2025-08-06 03:13:56 +0900

    Feat: 알림 이메일 인증용 메일 전송 로직

--- src/main/java/UMC/news/newsIntelligent/domain/mail/service/MailService.java
+++ src/main/java/UMC/news/newsIntelligent/domain/mail/service/MailService.java
@@ -18,13 +18,16 @@ import java.util.UUID;
 public class MailService {
 
     private final JavaMailSender mailSender;
+
     @Value("${spring.mail.username}") private String from;
+    // 도메인 연결 후 추가
+    // @Value("${app.front-url}") private String frontUrl;
 
     public String generateCode() {
         return String.format("%06d", new SecureRandom().nextInt(1_000_000));
     }
     public String generateToken() {
-        return UUID.randomUUID().toString().replace("-", "");   // 32 hex
+        return UUID.randomUUID().toString().replace("-", "");
     }
 
     public void sendOtpMail(String to, String code, String token, OtpCode.Type type) {
@@ -33,10 +36,14 @@ public class MailService {
 
         String html = """
             <div style='margin:80px;font-family:Verdana'>
-              <h2>NewsIntelligent 인증 메일입니다.</h2>
-              <p>아래 <b>코드</b>를 입력하거나 <b>매직링크</b>를 클릭하세요.</p>
-              <h1 style='color:#3366ff'>%s</h1>
-              <p><a href='%s'>➡️ 매직링크 바로가기</a> · 5분 내 1회 사용</p>
+                <section style="display: flex;">
+                    <h2 style='color:#07525F'>📨 NewsIntelligent</h2>
+                    &nbsp;&nbsp;
+                    <h2>인증 메일입니다.</h2>
+                </section>
+                <p>아래 <b>코드</b>를 입력하거나 <b>매직링크</b>를 클릭하세요.</p>
+                <h1 style='color:#07525F'>%s</h1>
+                <p><a href='%s' style="color: black;">➡️ 매직링크 바로가기</a> · 5분 내 1회 사용</p>
             </div>
             """.formatted(code, link);
 
@@ -44,6 +51,30 @@ public class MailService {
             MimeMessage msg = mailSender.createMimeMessage();
             msg.addRecipients(Message.RecipientType.TO, to);
             msg.setSubject("[NewsIntelligent] 인증 메일");
+            msg.setText(html, "utf-8", "html");
+            msg.setFrom(new InternetAddress(from, "NewsIntelligent"));
+            mailSender.send(msg);
+        } catch (Exception e) {
+            throw new IllegalStateException("메일 발송 실패", e);
+        }
+    }
+
+    public void sendNotificationEmailChangeMail(String to, String code, String token) {
+        String link  = "https://newsintelligent.io/settings/notification-email/magic?token=%s".formatted(token);
+
+        String html = """
+            <div style='margin:80px;font-family:Verdana'>
+              <h2>알림 이메일 변경 인증</h2>
+              <p>아래 <b>코드</b>를 입력하거나 <b>매직링크</b>를 클릭해 주세요.</p>
+              <h1 style='color:#07525F'>%s</h1>
+              <p><a href='%s' style="color: black;"> · 10분 내 1회 사용</p>
+            </div>
+            """.formatted(code, link);
+
+        try {
+            MimeMessage msg = mailSender.createMimeMessage();
+            msg.addRecipients(Message.RecipientType.TO, to);
+            msg.setSubject("[NewsIntelligent] 알림 이메일 변경 인증");
             msg.setText(html, "utf-8", "html");
             msg.setFrom(new InternetAddress(from, "NewsIntelligent"));
             mailSender.send(msg);
