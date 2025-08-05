59dd1cf1ad3cd3308a1188a785de54c8336678ba
caminobelllo <alstpgml0812@gmail.com>
2025-08-05 19:00:49 +0900

    Feat: MemberRepository에 중복 체크를 위한 메서드 추가

--- src/main/java/UMC/news/newsIntelligent/domain/member/repository/MemberRepository.java
+++ src/main/java/UMC/news/newsIntelligent/domain/member/repository/MemberRepository.java
@@ -1,12 +1,14 @@
 package UMC.news.newsIntelligent.domain.member.repository;
 
 import UMC.news.newsIntelligent.domain.member.entity.Member;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
     Optional<Member> findById(Long id);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
