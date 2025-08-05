06cfa60336423011e7718b75805e03913461a778
nomad1jin <yj007shin@gmail.com>
2025-08-06 02:31:05 +0900

    bug: 토픽 상세 페이지 조회 api

--- src/main/java/UMC/news/newsIntelligent/domain/news/controller/NewsController.java
+++ src/main/java/UMC/news/newsIntelligent/domain/news/controller/NewsController.java
@@ -2,9 +2,12 @@ package UMC.news.newsIntelligent.domain.news.controller;
 
 import UMC.news.newsIntelligent.domain.news.dto.NewsResponseDTO;
 import UMC.news.newsIntelligent.domain.news.service.NewsService;
+import UMC.news.newsIntelligent.domain.topic.dto.TopicResponseDTO;
+import UMC.news.newsIntelligent.domain.topic.service.query.TopicQueryService;
 import UMC.news.newsIntelligent.global.apiPayload.CustomResponse;
 import UMC.news.newsIntelligent.global.apiPayload.code.success.GeneralSuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -13,12 +16,23 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/topic")
-@Tag(name = "토픽 상세 페이지 - 기사 출처 관련 API", description = "출처 기사 조회")
+@Tag(name = "토픽 상세 페이지 조회 API", description = "출처 기사 조회")
 public class NewsController {
 
     private final NewsService newsService;
+    private final TopicQueryService topicQueryService;
 
-    @Operation(summary = "토픽 상세 페이지 - 출처 기사 목록 조회", description = "주제 생성에 사용된 출처 기사 목록을 반환하는 API입니다..")
+    @Operation(summary = "토픽 상세 페이지 조회", description = "토픽 상세 페이지를 조회하는 API입니다")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "토픽 상세 페이지 조회 성공")
+    })
+    @GetMapping("/{topicId}")
+    public CustomResponse<TopicResponseDTO.TopicDetailsResDTO> getTopicDetails(@PathVariable Long topicId){
+        TopicResponseDTO.TopicDetailsResDTO topicDetailsDTO = topicQueryService.getTopicById(topicId);
+        return CustomResponse.onSuccess(GeneralSuccessCode.GET_TOPIC, topicDetailsDTO);
+    }
+
+    @Operation(summary = "토픽 상세 페이지 - 출처 기사 목록 조회", description = "주제 생성에 사용된 출처 기사 목록을 반환하는 API입니다")
     @ApiResponses(value = {
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "토픽 출처 기사 목록 조회 성공")
     })
