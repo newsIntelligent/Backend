6959a07760fcdbbd2377fff563b0816ebdf2ab68
서가영 <alstpgml0812@gmail.com>
2025-08-14 20:53:34 +0900

    Chore: 불필요한 파일 삭제

--- src/main/java/UMC/news/newsIntelligent/domain/dailyReport/port/UnsubscribeTokenPort.java
+++
@@ -1,7 +0,0 @@
-package UMC.news.newsIntelligent.domain.dailyReport.port;
-
-import UMC.news.newsIntelligent.domain.member.entity.Member;
-
-public interface UnsubscribeTokenPort {
-	String issueToken(Member member);
-}
